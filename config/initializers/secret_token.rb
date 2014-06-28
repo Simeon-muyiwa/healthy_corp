c5559b003db00f62c7004a64b947837d22ccb5c9
Simeon Muyiwa <muyiwa100@yahoo.com>
2014-06-28 13:07:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthyCorp::Application.config.secret_key_base = 'bd52e44d9425715c728aa1f1681f11bfda7642f35f68cb3bc60d1b389d810f1acf333462b9005d14e1c5669718e5a7bb35d4342bb7801e2112e588b72c0fac4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HealthyCorp::Application.config.session_store :cookie_store, key: '_healthy_corp_session'
