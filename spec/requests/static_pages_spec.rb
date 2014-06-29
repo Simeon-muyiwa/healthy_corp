64aa360eb83df3788a8b6af16dad633f40e92fc1
Simeon Muyiwa <muyiwa100@yahoo.com>
2014-06-29 01:00:04 +0100

    adding home help and about to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,13 @@ describe "Static pages" do
 
 		it "should have the content 'Healthy Corps'" do
 			visit '/static_pages/home'
-			expect(page).to have_content('Healthy Corps')
+			expect(page).to have_selector('h1', :text => 'Healthy Corps')
+		end
+        
+         it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', 
+				     :text => "Health and Wellness Application | Home" )
 		end
 	end
 
@@ -15,7 +21,13 @@ describe "Static pages" do
 
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_content('Help')
+			expect(page).to have_selector('h1', :text => 'Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', 
+				     :text => "Health and Wellness Application | Help" )
 		end
 	end
 
@@ -23,7 +35,13 @@ describe "Static pages" do
 
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
+			expect(page).to have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title', 
+				    :text => "Health and Wellness Application | About Us" )
 		end
 	end
 end
