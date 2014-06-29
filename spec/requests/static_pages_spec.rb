069c3282b5e2ec01b9eb105613dc267cf7580e47
Simeon Muyiwa <muyiwa100@yahoo.com>
2014-06-29 17:42:19 +0100

    add contact page and and routing error re:'home page'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ describe "Static pages" do
 	describe "Home page" do
 
 		it "should have the content 'Healthy Corps'" do
-			visit '/static_pages/home'
+			visit '/'
 			expect(page).to have_selector('h1', :text => 'Healthy Corps')
 		end
         
          it "should have the right title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', 
 				     :text => "Health and Wellness Application | Home" )
 		end
@@ -42,6 +42,20 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
 				    :text => "Health and Wellness Application | About Us" )
+		end
+	end
+
+	describe "Contact Page" do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				  :text => "Health and Wellness Application | Contact" ) 
 		end
 	end
 end
