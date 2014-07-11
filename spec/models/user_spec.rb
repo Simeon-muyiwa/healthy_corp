7d4577dcb655efb6eb04ee57283966d867d38f64
Simeon Muyiwa <muyiwa100@yahoo.com>
2014-07-11 08:43:52 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   before do
   @user = User.new(name: "Example User", email: "user@example.com",
                    password: "foobar", password_confirmation: "foobar")
-end
+  end
 
   subject { @user }
 
@@ -15,6 +15,7 @@ end
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
@@ -98,5 +99,10 @@ end
        specify { expect(user_for_invalid_password).to be_false }
      end
    end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
 
